948bac47733a6fb0bd0b85302c36ab51bdaf5de8
marvlasa <martin.vlasa@gmail.com>
2021-05-13 22:35:53 -0300

    fixed like button and now is working fine

--- src/components/Like.jsx
+++ src/components/Like.jsx
@@ -20,23 +20,28 @@ function Like({ item, updateHomeReload }) {
         updateHomeReload();
       });
   };
-  function setHeartRed(itemId) {
-    if (item.likes.length === 1) {
-      document.getElementById(itemId).classList.remove("redHeart");
-    } else if (item.likes.length === 0) {
-      document.getElementById(itemId).classList.add("redHeart");
-    }
-  }
+  // function setHeartRed(itemId) {
+  //   if (item.likes.length === 1) {
+  //     document.getElementById(itemId).classList.remove("redHeart");
+  //   } else if (item.likes.length === 0) {
+  //     document.getElementById(itemId).classList.add("redHeart");
+  //   }
+  // }
+  // todo: change hardcode id for userId when login is made
+  const active = item.likes.includes("60859cfae2f6edc6a746d43f");
+
   return (
     <form onSubmit={handleSubmit} className="form-like">
       <input type="hidden" name="tweetId" value={item.author.id} />
       <button
         className="tweet-footer-btn button-like"
         type="submit"
-        onClick={() => setHeartRed(item._id)}
+        // onClick={() => setHeartRed(item._id)}
       >
         <i
-          className="octicon octicon-heart"
+          className={
+            active ? "octicon octicon-heart redHeart" : "octicon octicon-heart"
+          }
           aria-hidden="true"
           id={item._id}
         ></i>
